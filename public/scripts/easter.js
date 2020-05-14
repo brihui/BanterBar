481dfec5b944b25b797b43c2b5417e14ce5629ac
Jim Lin <jhlin5083@gmail.com>
2020-05-14 12:01:57 -0700

    Initial commit - Audio

---
+++ public/scripts/easter.js
@@ -0,0 +1,5 @@
+function easter(){
+    var audio = new Audio('../images/USSR.mp3');
+    audio.play;
+
+}
--- public/scripts/openForm.js
+++ public/scripts/openForm.js
@@ -1,7 +1,12 @@
 function openForm() {
+    easter();
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+}
+function easter(){
+    var x = document.getElementById("myAudio"); 
+    x.play();
 }
